c4f12b0f6d04098452eab3854d157af0ae696ede
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-12-23 19:11:05 +0700

    chore: add more des

--- products.js
+++ products.js
@@ -4,6 +4,7 @@ module.exports = [{
     description:
         `**1 Tháng:** 20k\n**3 Tháng:** 50k\n**6 Tháng:** 90k\n**12 Tháng:** 170k`
         + `\n\n**Thêm 1 slot:** 10k`
+        + '\n\n*Luôn luôn có sẵn.*'
         + `\n\n*Chỉ được xem profile đã cấp, không được đổi thông tin bao gồm ngôn ngữ, tên profile.`
         + ` Bảo hành đầy đủ, tối đa 3 lần mỗi tháng*`,
     color: 0xE1E2ED,
@@ -13,6 +14,7 @@ module.exports = [{
     url: 'https://cdn.discordapp.com/emojis/930409674287759360.webp?size=96&quality=lossless',
     description:
         `**1 Tháng:** 35k\n**3 Tháng:** 60k\n**6 Tháng:** 100k\n**12 Tháng:** 150k`
+        + '\n\nHầu hết là có sẵn hàng.'
         + `\n\n*Yêu cầu gửi tài khoản.`
         + ` Bảo hành đầy đủ, yêu cầu gửi tài khoản spotify khác **chưa add family** trong 12 tháng hoặc tài khoản mới.*`,
     color: 0xE1E2ED,
@@ -22,6 +24,7 @@ module.exports = [{
     url: 'https://cdn.discordapp.com/emojis/918832199367794698.webp?size=96&quality=lossless',
     description:
         `**1 Tháng:** 80k\n**1 Năm:** 600k`
+        + `\n\n*Hàng đặt trước, dạng **link**, có từ 1 đến 2 ngày*`
         + `\n\n*Đảm bảo chất lượng cao, tỉ lệ lỗi cực thấp.`
         + ` Bảo hành đầy đủ trong thời gian sử dụng*`,
     color: 0xE1E2ED,
