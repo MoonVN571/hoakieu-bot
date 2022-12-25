d8f025c5ecdc7503e1bc719273053b78b2a4df34
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-12-25 20:59:53 +0700

    feat: welcome message

---
+++ events/Bots/memberEvent.js
@@ -0,0 +1,16 @@
+const { Colors } = require('discord.js');
+const client = require('../../index');
+client.on('guildMemberAdd', member => {
+    client.channels.cache.get(client.config.logs.welcome)
+        .send({
+            content: '**Hi ' + member.user.toString() + '**',
+            embeds: [{
+                author: { name: member.user.tag, iconURL: member.displayAvatarURL() },
+                description: '**Welcome to ' + member.guild.name + '!**'
+                    + '\n\nXem bảng giá tại <#1055785937789001819>.'
+                    + '\nVào <#1055785251303075870> để pick role.',
+                color: Colors.DarkGrey,
+                timestamp: new Date().toISOString()
+            }]
+        });
+});
